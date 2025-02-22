cdeb2c0f838c3498e1937980055f2b526a876bbf
quhongqiang <1256618908@qq.com>
2025-02-22 13:35:16 +0800

    fix_pic

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
